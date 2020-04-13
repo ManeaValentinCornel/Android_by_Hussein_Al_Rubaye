d384e9325c3dc93b189cb7199a4e96bbb5b3f35f
Bobo <manea.valentin.cornel@gmail.com>
2020-04-13 23:32:39 +0100

    while, do while

---
+++ 00_Short_Kotlin_Course/10_While_DoWhile/src/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    //WHILE
+    println("******WHILE*********")
+    var i = 1
+
+    while (i <= 10) {
+        println("While  $i")
+        i+=2;
+    }
+    //DO WHILE
+    //RUN THE BLOCK AND CHECK THE CONDITION AFTER
+    println("********DO WHILE*********")
+    i = 1
+    do {
+        println("DO WHILE $i")
+        i+=2;
+
+    } while (i<=10)
+
+
+}
