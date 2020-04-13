2cdea1afd097dd89161da8e1f479074da1046194
Bobo <manea.valentin.cornel@gmail.com>
2020-04-13 06:55:17 +0100

    fun main(), println(), print() and "/n"

---
+++ 00_Short_Kotlin_Course/00_Print_Println_Breakline/src/Main.kt
@@ -0,0 +1,10 @@
+//startup point in kotlin
+fun main(){
+
+    print("Welcome to Kotlin \n")
+    //break line in kotlin
+    println("Valentin")
+    //second way of breaking a line using the println() method
+    print("How is you day ?")
+
+}
