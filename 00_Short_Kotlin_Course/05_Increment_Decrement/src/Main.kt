44f4a53d302b8e6a365a56dd5c5df9042add1cd3
Bobo <manea.valentin.cornel@gmail.com>
2020-04-13 10:41:07 +0100

    increment, decrement in kotlin --> basics

---
+++ 00_Short_Kotlin_Course/05_Increment_Decrement/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //Increment decrement in kotlin looks like the one in Java
+    var count = 1
+    print("Count : $count \n")
+    count += 1
+    print("Count : $count \n")
+    count += 3
+    print("Count : $count \n")
+    count -= 2
+    print("Count : $count \n")
+
+    //tricky output
+    var i = 5;
+    println(i++);
+    print(i--);
+
+}
